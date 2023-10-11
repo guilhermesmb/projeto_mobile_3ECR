937ac90ef1305848cce6b6840fd657128bd603be
ArthurTortoreli <arthurtortoreli@hotmail.com>
2023-10-10 22:45:37 -0300

    removing header

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false}}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Comanda" component={Comanda} />
       </Stack.Navigator>
